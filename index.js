f7eed43c6378608dc3a2debb2a3644d2ed03b2e3
Meredyth <mplack@credible.com>
2019-12-09 10:21:58 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+'use strict';
+
+// const apiKey = /*your API key here*/
+
+// const searchURL = 'https://newsapi.org/v2/everything';
+
+
+// function formatQueryParams(params) {
+//   const queryItems = Object.keys(params)
+//     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+//   return queryItems.join('&');
+// }
+
+function displayResults(responseJson) {
+  // if there are previous results, remove them
+  console.log(responseJson);
+  $('#results-list').empty();
+  // iterate through the articles array, stopping at the max number of results
+  for (let i = 0; i < responseJson.length ; i++){
+    // for each video object in the articles
+    //array, add a list item to the results 
+    //list with the article title, source, author,
+    //description, and image
+    $('#results-list').append(
+      `<li><h3><a href="${responseJson[i].html_url}">${responseJson[i].name}</a></h3>
+      </li>`
+    )};
+  //display the results section  
+  $('#results').removeClass('hidden');
+};
+
+function getNews(query) {
+  // const params = {
+  //   q: query,
+  //   language: "en",
+  // };
+  // const queryString = formatQueryParams(params)
+  const url = `https://api.github.com/users/${query}/repos`
+
+  console.log(url);
+
+  // const options = {
+  //   headers: new Headers({
+  //     "X-Api-Key": apiKey})
+  // };
+
+  fetch(url)
+    .then(response => { 
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    getNews(searchTerm);
+  });
+}
+
+$(watchForm);
